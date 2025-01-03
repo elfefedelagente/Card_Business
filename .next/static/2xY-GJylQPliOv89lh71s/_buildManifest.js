22ab96c9a1f444b207291135539705ea1d02c9f0
Cabaña Federico <federicogabriel12@hotmail.com>
2025-01-03 18:12:18 -0300

    npm run deploy y predeploy

---
+++ .next/static/2xY-GJylQPliOv89lh71s/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/510-250b2f4b3fbd1fbd.js","static/chunks/pages/index-5770d178a456e185.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
