5d70e9ea8f349e33a0941fecee57ae1b352f5814
Cabaña Federico <federicogabriel12@hotmail.com>
2025-01-03 20:48:44 -0300

    Cambios json y next.config

---
+++ .next/static/3idbu_CmFAEdbhVbcFbrT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/510-28caba43ab29b4bf.js","static/chunks/pages/index-ca9f2c3d8ae092ac.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- .next/static/lQ_6EoG7gzlRejvrbPqL-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/510-e5b5ad618151efca.js","static/chunks/pages/index-e0fe1e65372d5d37.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
